6b98e0be1a2d48c4927279f7cfa51666ef14d15c
nicolas-riera <habbo.rieranicolas08@gmail.com>
2026-06-18 14:55:42 +0200

    feat: added css loading for themes

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <gtk/gtk.h>
 
+static void apply_css(void) {
+    GtkCssProvider *provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+    
+    gtk_css_provider_load_from_path(provider, "styles/gtk.css");
+    
+    gtk_style_context_add_provider_for_display(
+        display,
+        GTK_STYLE_PROVIDER(provider),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+    
+    g_object_unref(provider);
+}
+
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
+
+    apply_css();
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "myDiscord");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
+
     gtk_window_present(GTK_WINDOW(window));
 }
 
